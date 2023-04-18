32054bcb0f8b8a9c2d02e775f9a019352431bfbe
manoj <gandhamanojkutty@gmail.com>
2023-04-18 18:11:16 +0530

    colour codes updated

--- user.sh
+++ user.sh
@@ -1,17 +1,40 @@
+echo -e "\e[33m>>>>>>>>> Setup NodeJS repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[33m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[33m>>>>>>>>> Add application User<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[33m>>>>>>>>> Create App Directory <<<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
+
+echo -e "\e[33m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+
+echo -e "\e[33m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
 unzip /tmp/user.zip
+
+echo -e "\e[33m>>>>>>>>> Install Npm <<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[33m>>>>>>>>> Copy user SystemD file <<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[33m>>>>>>>>> Start user Service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
+
+echo -e "\e[33m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[33m>>>>>>>>> Install MongoDB Client <<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[33m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
 mongo --host mongodb-dev.mdevops333.online </app/schema/user.js
 

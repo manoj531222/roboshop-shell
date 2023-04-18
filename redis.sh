19275b04bbb03a7485311f7a3147d3d69e84583f
manoj <gandhamanojkutty@gmail.com>
2023-04-18 14:43:53 +0530

    redis.sh updated

--- redis.sh
+++ redis.sh
@@ -6,6 +6,6 @@ echo -e "\e[36m>>>>>>>>> Install Redis <<<<<<<<\e[0m"
 yum install redis -y
 echo -e "\e[36m>>>>>>>>> change the config <<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
-cho -e "\e[36m>>>>>>>>> Start & Enable Redis Service <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Start & Enable Redis Service <<<<<<<<\e[0m"
 systemctl enable redis
 systemctl start redis

0d89a9ca63009363aa007c349734233b4ea8914c
manoj <gandhamanojkutty@gmail.com>
2023-04-20 13:56:38 +0530

    code updated

--- mysql.sh
+++ mysql.sh
@@ -8,4 +8,4 @@ echo -e "\e[36m>>>>>>>>> start mysql <<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl restart mysqld
 echo -e "\e[36m>>>>>>>>> reset mysql password <<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@

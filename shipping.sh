719458d117db00e56e90378e4e6ffd0792d03f30
manoj <gandhamanojkutty@gmail.com>
2023-04-20 19:38:51 +0530

    script_path

--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
 
 echo -e "\e[36m>>>>>>>>> install maven <<<<<<<<\e[0m"
 yum install maven -y
@@ -19,7 +20,7 @@ mv target/shipping-1.0.jar shipping.jar
 echo -e "\e[36m>>>>>>>>> install mysql <<<<<<<<\e[0m"
 yum install mysql -y
 echo -e "\e[36m>>>>>>>>> load scheema <<<<<<<<\e[0m"
-mysql -h mysql-dev.mdevops333.online -uroot -pRoboshop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.mdevops333.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
 echo -e "\e[36m>>>>>>>>> setup systemd service <<<<<<<<\e[0m"
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 echo -e "\e[36m>>>>>>>>> start shipping service <<<<<<<<\e[0m"

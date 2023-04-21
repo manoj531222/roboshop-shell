0a0181297e4bdeca5076dbefe7b8ea9af57e637b
manoj <gandhamanojkutty@gmail.com>
2023-04-21 12:06:48 +0530

    rabbitmq

--- rabitmq.sh
+++ rabitmq.sh
@@ -3,12 +3,6 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 
-if [ -z "$rabbitmq_appuser_password" ]; then
-  echo Input Roboshop Appuser Password Missing
-  exit
-fi
-
-
 echo -e "\e[36m>>>>>>>>> setup erlang repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 echo -e "\e[36m>>>>>>>>> setup rabbitmq repos <<<<<<<<\e[0m"

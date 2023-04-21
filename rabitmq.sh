466f7af67b61d038894ed3a885af19ae9c4211c9
manoj <gandhamanojkutty@gmail.com>
2023-04-21 12:05:48 +0530

    rabbitmq

--- rabitmq.sh
+++ rabitmq.sh
@@ -1,10 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-## rabbitmq_appuser_password=$1
-## ${rabbitmq_appuser_password}
 
-## roboshop123
 
 if [ -z "$rabbitmq_appuser_password" ]; then
   echo Input Roboshop Appuser Password Missing

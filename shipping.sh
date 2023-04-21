4f94f83e3d21588a26dab78ab64a148dc7089412
manoj <gandhamanojkutty@gmail.com>
2023-04-21 15:11:24 +0530

    code updated

--- shipping.sh
+++ shipping.sh
@@ -26,7 +26,7 @@ mv target/shipping-1.0.jar shipping.jar
 echo -e "\e[36m>>>>>>>>> Install MySQL <<<<<<<<\e[0m"
 yum install mysql -y
 echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
-mysql -h mysql-dev.rdevopsb72.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+mysql -h mysql-dev.mdevops333.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
 echo -e "\e[36m>>>>>>>>> Setup SystemD Service <<<<<<<<\e[0m"
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 echo -e "\e[36m>>>>>>>>> Start Shipping Service <<<<<<<<\e[0m"

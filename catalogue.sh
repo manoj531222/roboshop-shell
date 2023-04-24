7fc9415ffebc8f0885a7348b9b27f66de64aee0c
manoj <gandhamanojkutty@gmail.com>
2023-04-24 15:26:35 +0530

    code updated

--- catalogue.sh
+++ catalogue.sh
@@ -1,14 +1,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-component=catalogue
 
+component=catalogue
+schema_setup=mongo
 func_nodejs
 
-
-${script_path}Copy MongoDB repo <<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-${script_path}Install MongoDB Client <<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
-${script_path}Load Schema <<<<<<<<\e[0m"
-mongo --host mongodb-dev.mdevops333.online </app/schema/catalogue.js

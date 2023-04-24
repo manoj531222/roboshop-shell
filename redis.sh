06b5cdb91449cd8b22d0024db2f93b344dc453ab
manoj <gandhamanojkutty@gmail.com>
2023-04-24 15:33:16 +0530

    code updated

--- redis.sh
+++ redis.sh
@@ -2,13 +2,21 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-${script_path}Install Redis Repos <<<<<<<<\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-${script_path}Install Redis <<<<<<<<\e[0m"
-dnf module enable redis:remi-6.2 -y
-yum install redis -y
-${script_path}Update Redis Listen Address <<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
-${script_path}Start Redis Service <<<<<<<<\e[0m"
-systemctl enable redis
-systemctl restart redis
+
+func_print_head "Install Redis Repos"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$log_file
+func_stat_check $?
+
+func_print_head "Install Redis"
+dnf module enable redis:remi-6.2 -y &>>$log_file
+yum install redis -y &>>$log_file
+func_stat_check $?
+
+func_print_head "Update Redis Listen Address"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>$log_file
+func_stat_check $?
+
+func_print_head "Start Redis Service"
+systemctl enable redis &>>$log_file
+systemctl restart redis &>>$log_file
+func_stat_check $?
